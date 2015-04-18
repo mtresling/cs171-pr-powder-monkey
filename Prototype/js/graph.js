136591bfa164cd1f30a1f6e06ed1351537b1284a
mtresling <mtresling@g.harvard.edu>
2015-04-17 23:23:33 -0400

    Mileston Submission

--- Prototype/js/graph.js
+++ Prototype/js/graph.js
@@ -1,12 +1,16 @@
-graph = function(_data){
+Graph = function(_data, _x_prop, _y_prop, _svg, _width){
 	this.data = _data;
+	this.x_prop = x_prop;
+	this.y_prop = -y_prop;
+	this.svg = _svg;
+	this.width = _width;
 	this.initGraph();
 }
 
-graphObj.prototype.initGraph = function(){
-		
+Graph.prototype.initGraph = function(){
+	
 };
 
-graphObj.prototype.updateGraph = function(){
+Graph.prototype.updateGraph = function(){
 		
 };
